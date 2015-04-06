1061c5885044aa322158530f053d56200cb173a5
Chi-Han Wang <cwang25@ncsu.edu>
2015-04-06 02:09:21 -0400

    DailyDateSummary food diary bean.

--- iTrust/src/edu/ncsu/csc/itrust/beans/FoodDiaryDateSummaryBean.java
+++ iTrust/src/edu/ncsu/csc/itrust/beans/FoodDiaryDateSummaryBean.java
@@ -15,6 +15,8 @@ public class FoodDiaryDateSummaryBean extends FoodDiaryBean {
 
 	/**
 	 * The constructor of daily summary food diary. 
+	 * Since its daily summary of food diaries, so there is no food name, no serving number, and no meal type.
+	 * The servCal will then present as total calories for that day.
 	 * @param oID OwnerID
 	 * @param d Date
 	 * @param servCal
@@ -25,20 +27,106 @@ public class FoodDiaryDateSummaryBean extends FoodDiaryBean {
 	 * @param fiberg
 	 * @param proteing
 	 */
-	public FoodDiaryDateSummaryBean(long oID, Date d, MealTypes mealType,
-			String foodName, double servNum, double servCal, double fatg,
+	public FoodDiaryDateSummaryBean(long oID, Date d, double servCal, double fatg,
 			double sodiummg, double carbsg, double sugarg, double fiberg,
 			double proteing) {
-		super(oID, d, mealType, foodName, servNum, servCal, fatg, sodiummg,
+		super(oID, d, null, null, -1, servCal, fatg, sodiummg,
 				carbsg, sugarg, fiberg, proteing);
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#getNameOfFood()
 	 */
-	public FoodDiaryDateSummaryBean() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public String getNameOfFood() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("getNameOfFood for FoodDiaryDateSummaryBean");
 	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#setNameOfFood(java.lang.String)
+	 */
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public void setNameOfFood(String nameOfFood) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setNameOfFood for FoodDiaryDateSummaryBean");
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#getNumberOfServings()
+	 */
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public double getNumberOfServings() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("getNumberOfServings for FoodDiaryDateSummaryBean");
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#setNumberOfServings(double)
+	 */
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public void setNumberOfServings(double numberOfServings) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setNumberOfServings for FoodDiaryDateSummaryBean");
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#getRowID()
+	 */
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public long getRowID() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("getRowID for FoodDiaryDateSummaryBean");
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#setRowID(long)
+	 */
+	/**
+	 * unsupport method for FoodDiaryDateSummaryBean.
+	 * @return
+	 */
+	@Override
+	public void setRowID(long rowID) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setRowID for FoodDiaryDateSummaryBean");
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc.itrust.beans.FoodDiaryBean#totalCalories()
+	 */
+	/**
+	 *
+	 * FoodDiaryDateSummaryBean will use caloriesPerServing as total calories.
+	 * @return
+	 */
+	@Override
+	public double totalCalories() {
+		// TODO Auto-generated method stub
+		return this.getCaloriesPerServing();
+	}
+
+	
 
 }
