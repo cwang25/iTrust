5a196b4bf4e90fd825921ae509086d16c6063c89
Miles Wells <mcwells2@ncsu.edu>
2015-04-08 19:41:24 -0400

    Added BBT and selenium for HCP for UC 71

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/FoodDiaryDateFilterTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/FoodDiaryDateFilterTest.java
@@ -48,4 +48,55 @@ public class FoodDiaryDateFilterTest extends iTrustSeleniumTest {
 	    driver.findElement(By.id("dateFilterSubmit")).click();
 	    assertTrue(pageContains("Invalid date. Please make sure your dates are valid and in MM/dd/yyyy format."));
 	}
+
+	@Test
+	public void testNutritionistFilterSuccess() throws Exception {
+		driver.setJavascriptEnabled(true);
+		login("9900000012", "pw");
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+	    driver.findElement(By.linkText("Patient Food Diary")).click();
+	    driver.findElement(By.id("searchBox")).clear();
+	    driver.findElement(By.id("searchBox")).sendKeys("500");
+	    driver.findElement(By.xpath("//input[@value='500' and @type='button']")).click();
+	    driver.findElement(By.id("dateRangeFrom")).clear();
+	    driver.findElement(By.id("dateRangeFrom")).sendKeys("04/13/2014");
+	    driver.findElement(By.id("dateFilterSubmit")).click();
+	    assertFalse(driver.findElementsByXPath("//*[contains(text(), '5/21/2013')]").get(0).isDisplayed());
+	    driver.setJavascriptEnabled(false);
+	}
+	
+	@Test
+	public void testNutritionistFilterRangeSuccess() throws Exception {
+		driver.setJavascriptEnabled(true);
+		login("9900000012", "pw");
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+	    driver.findElement(By.linkText("Patient Food Diary")).click();
+	    driver.findElement(By.id("searchBox")).clear();
+	    driver.findElement(By.id("searchBox")).sendKeys("500");
+	    driver.findElement(By.xpath("//input[@value='500' and @type='button']")).click();
+	    driver.findElement(By.id("dateRangeCheckbox")).click();
+	    driver.findElement(By.id("dateRangeFrom")).clear();
+	    driver.findElement(By.id("dateRangeFrom")).sendKeys("04/13/2014");
+	    driver.findElement(By.id("dateRangeTo")).clear();
+	    driver.findElement(By.id("dateRangeTo")).sendKeys("05/22/2013");
+	    driver.findElement(By.id("dateFilterSubmit")).click();
+	    assertFalse(driver.findElementsByXPath("//*[contains(text(), '5/21/2013')]").get(0).isDisplayed());
+	    driver.setJavascriptEnabled(false);
+	}
+	
+	@Test
+	public void testNutritionistDateValidation() throws Exception {
+		driver.setJavascriptEnabled(true);
+		login("9900000012", "pw");
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+	    driver.findElement(By.linkText("Patient Food Diary")).click();
+	    driver.findElement(By.id("searchBox")).clear();
+	    driver.findElement(By.id("searchBox")).sendKeys("500");
+	    driver.findElement(By.xpath("//input[@value='500' and @type='button']")).click();
+	    driver.findElement(By.id("dateRangeFrom")).clear();
+	    driver.findElement(By.id("dateRangeFrom")).sendKeys("04/13/14");
+	    driver.findElement(By.id("dateFilterSubmit")).click();
+	    assertTrue(pageContains("Invalid date. Please make sure your dates are valid and in MM/dd/yyyy format."));
+	    driver.setJavascriptEnabled(false);
+	}
 }
