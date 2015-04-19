ca2b109f831786a50c57f69ac94492be0b39be09
Chi-Han Wang <cwang25@ncsu.edu>
2015-04-19 05:54:00 -0400

    Fixed PendingApptTest.

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/PendingApptTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/PendingApptTest.java
@@ -1,74 +1,113 @@
 package edu.ncsu.csc.itrust.seleniumtests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import static org.junit.Assert.*;
-import org.openqa.selenium.NoSuchElementException;
-
+/**
+ * Use Case 42
+ */
 public class PendingApptTest extends iTrustSeleniumTest {
-	
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
 		gen.clearAllTables();
 		gen.standardData();
 		gen.pendingAppointmentAlert();
+		driver = new HtmlUnitDriver();
+		baseUrl = "http://localhost:8080";
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
-	
-	@Test
-	public void testPendingAppointmentAlert () throws Exception {
-		login("9000000000", "pw");
-		assertTrue(driver.getPageSource().contains("Appointment requests."));
+
+	public void testPendingAppointmentAlert() throws Exception {
+		driver.get(baseUrl + "/iTrust/auth/forwardUser.jsp");
+		driver.findElement(By.id("j_username")).clear();
+		driver.findElement(By.id("j_username")).sendKeys("9000000000");
+		driver.findElement(By.id("j_password")).clear();
+		driver.findElement(By.id("j_password")).sendKeys("pw");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		assertTextPresent("Welcome, Kelly Doctor", driver);
+		assertTextPresent("Appointment requests.", driver);
 	}
-	
-	@Test
+
+	private void assertTextPresent(String str, WebDriver driver2) {
+		// TODO Auto-generated method stub
+		assertTrue(driver2.getPageSource().contains(str));
+	}
+
 	public void testAcceptAnAppointment() throws Exception {
 		gen.clearAllTables();
 		gen.standardData();
 		gen.pendingAppointmentAlert();
-		
-		login("9000000000", "pw");
+
+		driver.get(baseUrl + "/iTrust/auth/forwardUser.jsp");
+		driver.findElement(By.id("j_username")).clear();
+		driver.findElement(By.id("j_username")).sendKeys("9000000000");
+		driver.findElement(By.id("j_password")).clear();
+		driver.findElement(By.id("j_password")).sendKeys("pw");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		assertTextPresent("Appointment requests.", driver);
+
 		driver.findElement(By.linkText("Appointment Requests")).click();
 		driver.findElement(By.linkText("Approve")).click();
-		driver.findElement(By.xpath("//a[text() = 'Home']")).click();
-		
-		assertFalse(driver.getPageSource().contains("Appointment requests."));
+		driver.get(baseUrl + "/iTrust/auth/hcp/home.jsp");
+		assertTextPresent("No appointment requests.", driver);
 	}
-	
-	@Test
+
 	public void testConflictingAppt() throws Exception {
 		gen.clearAllTables();
 		gen.standardData();
 		gen.pendingAppointmentConflict();
-		
-		login("9000000000", "pw");
+
+		driver.get(baseUrl + "/iTrust/auth/forwardUser.jsp");
+		driver.findElement(By.id("j_username")).clear();
+		driver.findElement(By.id("j_username")).sendKeys("9000000000");
+		driver.findElement(By.id("j_password")).clear();
+		driver.findElement(By.id("j_password")).sendKeys("pw");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		assertTrue(driver.findElements(By.linkText("2")).size() > 0);
+
 		driver.findElement(By.linkText("Appointment Requests")).click();
 		driver.findElement(By.linkText("Approve")).click();
-		driver.findElement(By.xpath("//a[text() = 'Home']")).click();
-		driver.findElement(By.linkText("1"));
-		try {
-			driver.findElement(By.linkText("2"));
-			fail("Expected NoSuchElementException.");
-		} catch (NoSuchElementException nsee) {
-			assertTrue(true); // Expected exception
-		}
+		driver.get(baseUrl + "/iTrust/auth/hcp/home.jsp");
+		assertTrue(driver.findElements(By.linkText("2")).size() == 0);
+		assertTrue(driver.findElements(By.linkText("1")).size() > 0);
 	}
-	
-	@Test
-	public void testDeclineAnAppointment() throws Exception{
+
+	public void testDeclineAnAppointment() throws Exception {
 		gen.clearAllTables();
 		gen.standardData();
 		gen.pendingAppointmentAlert();
-		
-		login("9000000000", "pw");
+
+		driver.get(baseUrl + "/iTrust/auth/forwardUser.jsp");
+		driver.findElement(By.id("j_username")).clear();
+		driver.findElement(By.id("j_username")).sendKeys("9000000000");
+		driver.findElement(By.id("j_password")).clear();
+		driver.findElement(By.id("j_password")).sendKeys("pw");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		assertTextPresent("Appointment requests.", driver);
+
 		driver.findElement(By.linkText("Appointment Requests")).click();
 		driver.findElement(By.linkText("Reject")).click();
-		driver.findElement(By.xpath("//a[text() = 'Home']")).click();
-		
-		assertTrue(driver.getPageSource().contains("No appointment requests."));
+		driver.get(baseUrl + "/iTrust/auth/hcp/home.jsp");
+		assertTextPresent("No appointment requests.", driver);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
 	}
 }
-
