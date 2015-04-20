ca01d6205dfaa94cb56fb69b7a81fc0f989a4fa0
ngrodrig <ngrodrig@ncsu.edu>
2015-04-20 19:10:17 -0400

    Added tests for macronutrients and suggestions (selenium)

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/FoodDiarySuggestionsTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/FoodDiarySuggestionsTest.java
@@ -1,20 +1,26 @@
-/**
- * 
- */
 package edu.ncsu.csc.itrust.seleniumtests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
+import edu.ncsu.csc.itrust.beans.FoodDiaryLabelBean;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
+import edu.ncsu.csc.itrust.dao.mysql.FoodDiaryLabelDAO;
+import edu.ncsu.csc.itrust.testutils.TestDAOFactory;
 
 /**
  * @author nishant
  *
  */
-public class AddSuggestionToFoodDiary extends iTrustSeleniumTest {
-
+public class FoodDiarySuggestionsTest extends iTrustSeleniumTest {
+	private DAOFactory factory = TestDAOFactory.getTestInstance();
+	private FoodDiaryLabelDAO  fdlDAO = factory.getFoodDiaryLabelDAO();
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -30,31 +36,39 @@ public class AddSuggestionToFoodDiary extends iTrustSeleniumTest {
 	 */
 	@Test
 	public void testViewSuggestion() throws Exception {
+		//Login as Spencer Reid
 		login("9900000012", "pw");
 		driver.findElement(By.cssSelector("h2.panel-title")).click();
 		driver.findElement(By.linkText("Patient Food Diary")).click();
+		//Search for patient 500 Aaron Hotchner
 		driver.findElement(By.id("searchBox")).clear();
 		driver.findElement(By.id("searchBox")).sendKeys("500");
 		//waitFor(2);
-		driver.findElement(By.xpath("//input[@value='500' and @type='button']")).click();
+		clickOnJavascriptElement(By.xpath("//input[@value='500' and @type='button']"));
+		//Click on suggestion plus button
 		driver.findElement(By.id("toggle1")).click();
-		System.out.println(driver.getPageSource());
+		//Add suggestion
 		driver.findElement(By.id("suggestionText1")).clear();
 		driver.findElement(By.id("suggestionText1")).sendKeys("This is a test suggestion");
 		driver.findElement(By.id("addNewSuggestion1")).click();
+		//Logout
 		clickOnNonJavascriptElement(driver.findElement(By.id("logoutBtn")));
+		
+		//Login as Aaron Hotchner (500)
 		driver.findElement(By.id("j_username")).click();
 		driver.findElement(By.id("j_username")).clear();
 		driver.findElement(By.id("j_username")).sendKeys("500");
 		driver.findElement(By.id("j_password")).clear();
 		driver.findElement(By.id("j_password")).sendKeys("pw");
 		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		//Click on 'View' panel
 		driver.findElement(By.xpath("//div[@id='iTrustMenu']/div/div[2]/div/h2")).click();
+		//Click on 'My Food Diary'
 		driver.findElement(By.linkText("My Food Diary")).click();
 		driver.findElement(By.id("toggle1")).click();
+		assertTrue(driver.findElement(By.id("tarea1")).getText().contains("This is a test suggestion"));
 		driver.findElement(By.id("toggle1")).click();
-		driver.findElement(By.id("toggle1")).click();
-		driver.findElement(By.id("toggle1")).click();
+		//Logout
 		clickOnNonJavascriptElement(driver.findElement(By.id("logoutBtn")));
 	}
 
@@ -132,5 +146,63 @@ public class AddSuggestionToFoodDiary extends iTrustSeleniumTest {
 		assertTrue(driver.findElement(By.id("tarea1")).getText().contains("AprilSuggestion"));
 		driver.findElement(By.id("toggle2")).click();
 		assertTrue(driver.findElement(By.id("tarea2")).getText().contains("MaySuggestion"));
+	}
+	
+	/**
+	 * Test editing a suggestion
+	 * @throws Exception 
+	 */
+	@Test
+	public void testEditSuggestion() throws Exception{
+		//Login as Spencer Reid
+		login("9900000012", "pw");
+		//Click on Patient -> Patient Food Diary
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+	    driver.findElement(By.linkText("Patient Food Diary")).click();
+	    //Enter 500 in search box
+	    driver.findElement(By.id("searchBox")).clear();
+	    driver.findElement(By.id("searchBox")).sendKeys("500");
+	    clickOnJavascriptElement(By.xpath("//input[@value='500' and @type='button']"));
+	    //Click add suggestion plus button
+	    driver.findElement(By.id("toggle1")).click();
+	    //Add a new suggestion
+	    driver.findElement(By.id("suggestionText1")).clear();
+	    driver.findElement(By.id("suggestionText1")).sendKeys("This is a test suggestion that I will edit.");
+	    driver.findElement(By.id("addNewSuggestion1")).click();
+	    //Click add suggestion plus button
+	    driver.findElement(By.id("toggle1")).click();
+	    //Select the newly added suggestion
+	    new Select(driver.findElement(By.id("savedSuggestionList1"))).selectByVisibleText("This is a test suggestion");
+	    driver.findElement(By.id("tarea1")).clear();
+	    driver.findElement(By.id("tarea1")).sendKeys("This is a test suggestion that I will edit. I edited it.");
+	    //Click update
+	    driver.findElement(By.id("updateSuggestion1")).click();
+	    driver.executeScript("window.confirm = function(msg){return true;}");
+	    assertTrue(pageContains("I edited it"));
+	}
+	
+	@Test
+	public void testDeleteSuggestion() throws Exception{
+		login("9900000012", "pw");
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+	    driver.findElement(By.linkText("Patient Food Diary")).click();
+	    driver.findElement(By.id("searchBox")).clear();
+	    driver.findElement(By.id("searchBox")).sendKeys("500");
+	    clickOnJavascriptElement(By.xpath("//input[@value='500' and @type='button']"));
+	    driver.findElement(By.id("toggle1")).click();
+	    driver.findElement(By.id("suggestionText1")).clear();
+	    driver.findElement(By.id("suggestionText1")).sendKeys("Add suggestion for deleting");
+	    driver.findElement(By.id("addNewSuggestion1")).click();
+	    driver.findElement(By.id("toggle1")).click();
+	    new Select(driver.findElement(By.id("savedSuggestionList1"))).selectByVisibleText("Add suggestion for deleti");
+	    driver.findElement(By.id("removeSuggestion1")).click();
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    js.executeScript("window.confirm = function(msg){return true;}");
+
+	    //Selenium throws exception. So we check label has been deleted like this:
+	    List<FoodDiaryLabelBean> labels = fdlDAO.getAllFoodDiaryLabels(500L);
+	    for(FoodDiaryLabelBean label : labels){
+	    	assertNotEquals("Add suggestion for deleting", label.getLabel());
+	    }
 	}
 }
--- iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/MacroNutrientCalculatorTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/seleniumtests/MacroNutrientCalculatorTest.java
@@ -3,10 +3,12 @@ package edu.ncsu.csc.itrust.seleniumtests;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 
 /**
- * @author nishant
- *
+ * HtmlUnitDriver does not support Chart.js. So most test cases in this class use Javascript hacks
+ * to test expected and actual results without actually drawing the chart. Please note that 
+ * macronutrient chart functionality has been thoroughly tested manually and through the FirefoxDriver.
  */
 public class MacroNutrientCalculatorTest extends iTrustSeleniumTest{
 	/**
@@ -18,9 +20,12 @@ public class MacroNutrientCalculatorTest extends iTrustSeleniumTest{
 		gen.standardData();
 	}
 	
-	
+	/**
+	 * Tests adding a valid macronutrient chart entry
+	 * @throws Exception
+	 */
 	@Test
-	public void test() throws Exception {
+	public void testAddValidEntry() throws Exception {
 		login("1", "pw");
 	    
 		//Click on 'View'
@@ -48,6 +53,179 @@ public class MacroNutrientCalculatorTest extends iTrustSeleniumTest{
 	    //Select Activity Level as 'Lightly active'
 	    driver.findElement(By.id("light")).click();
 	    //Click on Preview
-	    driver.findElement(By.id("preview")).click();
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    //Simulate clicking the Preview Button. Since using driver.findElement.click() does not work, we have to use this javascript hack
+	    assertEquals(new Boolean(true), (Boolean)js.executeScript("return validateInput();"));
+	}
+	
+	/**
+	 * Tests adding an invalid weight entry
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddInvalidWeight1() throws Exception{
+		//Login as patient 1
+		login("1", "pw");
+		//Click on 'View' in the sidebar
+	    driver.findElement(By.xpath("//div[@id='iTrustMenu']/div/div[2]/div/h2")).click();
+	    //Click on 'My Food Diary'
+	    driver.findElement(By.linkText("My Food Diary")).click();
+	    //Click on 'Macro Calculator'
+	    driver.findElement(By.id("macroBtn")).click();
+	    //Select Male
+	    driver.findElement(By.id("male")).click();
+	    driver.findElement(By.id("age")).clear();
+	    //Add age 30
+	    driver.findElement(By.id("age")).sendKeys("30");
+	    driver.findElement(By.id("weight")).clear();
+	    //Add weight 'seventy' -- invalid
+	    driver.findElement(By.id("weight")).sendKeys("seventy");
+	    driver.findElement(By.id("height")).clear();
+	    driver.findElement(By.id("height")).sendKeys("170");
+	    //Click maintain weight
+	    driver.findElement(By.id("maintainmode")).click();
+	    driver.findElement(By.id("sedentary")).click();
+	    
+	    
+	    //driver.findElement(By.id("preview")).click();
+	    //Click 'Save
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    //Simulate clicking the Preview Button. Since using driver.findElement.click() does not work, we have to use this javascript hack
+	    js.executeScript("graphIt();");
+	    //driver.findElement(By.id("preview")).click();
+	    String errorMsg = (String)js.executeScript("return document.getElementById('errorTextWeight').innerHTML;");
+	    
+	    assertEquals("Enter value in range 1-790", errorMsg);
+	    //Logout
+	    logout();
+	}
+	
+	/**
+	 * Tests adding another invalid weight entry
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddInvalidWeight2() throws Exception{
+		//Login as patient 1
+		login("1", "pw");
+		//Click on 'View' in the sidebar
+	    driver.findElement(By.xpath("//div[@id='iTrustMenu']/div/div[2]/div/h2")).click();
+	    //Click on 'My Food Diary'
+	    driver.findElement(By.linkText("My Food Diary")).click();
+	    //Click on 'Macro Calculator'
+	    driver.findElement(By.id("macroBtn")).click();
+	    //Select Male
+	    driver.findElement(By.id("male")).click();
+	    driver.findElement(By.id("age")).clear();
+	    //Add age 30
+	    driver.findElement(By.id("age")).sendKeys("30");
+	    driver.findElement(By.id("weight")).clear();
+	    //Add weight '900' -- invalid
+	    driver.findElement(By.id("weight")).sendKeys("900");
+	    driver.findElement(By.id("height")).clear();
+	    //Add height 170
+	    driver.findElement(By.id("height")).sendKeys("170");
+	    //Click maintain weight
+	    driver.findElement(By.id("maintainmode")).click();
+	    driver.findElement(By.id("sedentary")).click();
+	    
+	    
+	    //driver.findElement(By.id("preview")).click();
+	    //Click 'Save
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    //Simulate clicking the Preview Button. Since using driver.findElement.click() does not work, we have to use this javascript hack
+	    js.executeScript("graphIt();");
+	    //driver.findElement(By.id("preview")).click();
+	    String errorMsg = (String)js.executeScript("return document.getElementById('errorTextWeight').innerHTML;");
+	    
+	    assertEquals("Enter value in range 1-790", errorMsg);
+	    //Logout
+	    logout();
+	}
+	
+	/**
+	 * Tests adding an invalid height entry
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddInvalidHeight1() throws Exception{
+		//Login as patient 1
+		login("1", "pw");
+		//Click on 'View' in the sidebar
+	    driver.findElement(By.xpath("//div[@id='iTrustMenu']/div/div[2]/div/h2")).click();
+	    //Click on 'My Food Diary'
+	    driver.findElement(By.linkText("My Food Diary")).click();
+	    //Click on 'Macro Calculator'
+	    driver.findElement(By.id("macroBtn")).click();
+	    //Select Male
+	    driver.findElement(By.id("male")).click();
+	    driver.findElement(By.id("age")).clear();
+	    //Add age 30
+	    driver.findElement(By.id("age")).sendKeys("30");
+	    driver.findElement(By.id("weight")).clear();
+	    //Add weight 70
+	    driver.findElement(By.id("weight")).sendKeys("70");
+	    driver.findElement(By.id("height")).clear();
+	    //Add height 274 -- invalid
+	    driver.findElement(By.id("height")).sendKeys("274");
+	    //Click maintain weight
+	    driver.findElement(By.id("maintainmode")).click();
+	    driver.findElement(By.id("sedentary")).click();
+	    
+	    
+	    //driver.findElement(By.id("preview")).click();
+	    //Click 'Save
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    //Simulate clicking the Preview Button. Since using driver.findElement.click() does not work, we have to use this javascript hack
+	    js.executeScript("graphIt();");
+	    //driver.findElement(By.id("preview")).click();
+	    String errorMsg = (String)js.executeScript("return document.getElementById('errorTextHeight').innerHTML;");
+	    
+	    assertEquals("Enter value in range: 1-273", errorMsg);
+	    //Logout
+	    logout();
+	}
+	
+	/**
+	 * Tests adding another invalid height entry
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddInvalidHeight2() throws Exception{
+		//Login as patient 1
+		login("1", "pw");
+		//Click on 'View' in the sidebar
+	    driver.findElement(By.xpath("//div[@id='iTrustMenu']/div/div[2]/div/h2")).click();
+	    //Click on 'My Food Diary'
+	    driver.findElement(By.linkText("My Food Diary")).click();
+	    //Click on 'Macro Calculator'
+	    driver.findElement(By.id("macroBtn")).click();
+	    //Select Male
+	    driver.findElement(By.id("male")).click();
+	    driver.findElement(By.id("age")).clear();
+	    //Add age 30
+	    driver.findElement(By.id("age")).sendKeys("30");
+	    driver.findElement(By.id("weight")).clear();
+	    //Add weight '70
+	    driver.findElement(By.id("weight")).sendKeys("70");
+	    driver.findElement(By.id("height")).clear();
+	    //Add height 'tall' - invalid
+	    driver.findElement(By.id("height")).sendKeys("tall");
+	    //Click maintain weight
+	    driver.findElement(By.id("maintainmode")).click();
+	    driver.findElement(By.id("sedentary")).click();
+	    
+	    
+	    //driver.findElement(By.id("preview")).click();
+	    //Click 'Save
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    //Simulate clicking the Preview Button. Since using driver.findElement.click() does not work, we have to use this javascript hack
+	    js.executeScript("graphIt();");
+	    //driver.findElement(By.id("preview")).click();
+	    String errorMsg = (String)js.executeScript("return document.getElementById('errorTextHeight').innerHTML;");
+	    
+	    assertEquals("Enter value in range: 1-273", errorMsg);
+	    //Logout
+	    logout();
 	}
 }
